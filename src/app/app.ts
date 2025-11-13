acd21ea366f2983f41f660a8d5d00be5953b97f0
yaquitadeveloper <yaquitadeveloper@gmail.com>
2025-11-12 20:20:30 -0500

    incorporar logica y vista en el app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsListComponent } from './products/products-list/products-list';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsListComponent, HttpClient],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('ecommerce-angular-bun');
